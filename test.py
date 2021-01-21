d6e7e97e973c7f5c8ec385d7b95d87c5b0b59207
paperspace <adpl33@gmail.com>
2021-01-21 18:03:00 -0500

    training script ok

---
+++ test.py
@@ -0,0 +1,71 @@
+import os
+import pandas as pd
+from glob import glob
+from PIL import Image
+import torch
+from torch.utils.data import (
+    DataLoader,
+    Dataset
+)
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+
+class CustomDataSet(Dataset):
+    def __init__(self, main_dir, transform):
+        self.main_dir = main_dir
+        self.transform = transform
+        all_imgs = os.listdir(main_dir)
+        self.total_imgs = all_imgs
+
+    def __len__(self):
+        return len(self.total_imgs)
+
+    def __getitem__(self, idx):
+        img_loc = os.path.join(self.main_dir, self.total_imgs[idx])
+        image = Image.open(img_loc).convert("RGB")
+        tensor_image = self.transform(image)
+        return tensor_image
+
+if __name__ == "__main__":
+    input_dir = "data/test_input/images"
+
+    # create output dataframe
+    res = pd.DataFrame(columns=('ID', 'Target'))
+    
+    # set transforms
+    tfms = transforms.Compose([transforms.ToTensor()])
+
+    # set model
+    model = torchvision.models.resnet34(pretrained=False) 
+    model.fc = torch.nn.Linear(model.fc.in_features, 1)
+    model.load_state_dict(torch.load("model_test.pth"))
+    model.eval()
+
+    dirs = glob(input_dir+"/*/")    
+    # for each id
+    for directory in dirs:
+        # parse id
+        ID = int(directory.split("/")[-2].replace("ID_", ""))
+        print(ID)
+
+        dataset = CustomDataSet(directory, tfms)
+        loader = DataLoader(dataset, batch_size=3, shuffle=False)
+        
+        positive = False
+        for _, images in enumerate(loader):
+            images = images.to(device="cuda")
+            
+            scores = model(images)
+            print(scores)
+            scores = scores > 0.5 
+            print(scores)
+            any_positives = torch.any(scores).item()
+            print(any_positives)   
+            break
+        break
+                 
+        # print("files ", files)
+            # run model
+            # write output to dataframe
+    
--- train.py
+++ train.py
@@ -1,7 +1,9 @@
 import torch
+import wandb
 import torchvision
 import torch.nn as nn
 import torch.optim as optim
+from utils import AverageMeter
 import torch.nn.functional as F
 from torch.utils.data import (
     DataLoader,
@@ -10,25 +12,40 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms 
 
 # Check accuracy on training & test to see how good our model
+def get_accuracy(y_true, y_prob):
+    assert y_true.ndim == 1 and y_true.size() == y_prob.size()
+    y_prob = y_prob > 0.5
+    return (y_true == y_prob).sum().item() / y_true.size(0)
+
 def evaluate(loader, model):
     print("Evaluate")
+
     # Set model to eval
     model.eval()
 
+    acc_mean = None
     with torch.no_grad():
         for batch_idx, (x, y) in enumerate(loader):
             x = x.to(device=device)
-            y=y/config.max_target
-            y = y.to(device=device)
+            y = y.to(device=device).to(torch.float32)
+            y = torch.unsqueeze(y, 1)
 
             scores = model(x)
-            loss = criterion(scores, y.float().unsqueeze(1))
-            test_loss.update(float(loss.item())*config.max_target)
+            loss = criterion(scores, y)
 
-        wandb.log({
-            "valid_loss": test_loss.avg
-        })
-        test_loss.reset()
+            y = torch.squeeze(y)
+            scores = torch.squeeze(scores)
+            acc = get_accuracy(y, scores)
+
+            if acc_mean is None:
+                acc_mean = acc
+            else:
+                acc_mean = (acc_mean + acc)/2
+    
+    wandb.log({
+    "valid_acc": acc_mean,
+    "valid_loss": loss.item()
+    })
 
     # Set model back to train
     model.train()
@@ -40,8 +57,8 @@ if __name__ == "__main__":
         batch_size = 64,
         num_epochs = 25,
         model_depth = 50,
-        aug = true,
-        pretrained = true
+        aug = True,
+        pretrained = True
     )
 
     run = wandb.init(project="owkin-chal", job_type='train', config=hyperparameters_defaults)
@@ -50,25 +67,78 @@ if __name__ == "__main__":
     # init model
     device = torch.device("cuda")
     model_name = "resnet{depth}".format(depth=config.model_depth)
-    model = torchivision.models.__dict__[model_name](pretrained=config.pretrained)
-    model.fc = torch.nn.linear(512, 1)
+    model = torchvision.models.__dict__[model_name](pretrained=config.pretrained)
+    model.fc = torch.nn.Linear(model.fc.in_features, 1)
     
-    model = model.to(device)
-
     # set transforms
     augs = [transforms.ToTensor()]
 
     if config.aug:
-        augs.append(transforms.RandomHorizontalFlip(p=0.5)
-        augs.append(transforms.RandomVerticalFlip(p=0.5)
+        augs.append(transforms.RandomHorizontalFlip(p=0.5))
+        augs.append(transforms.RandomVerticalFlip(p=0.5))
 
-    tfms = transforms.Compose[augs]
+    tfms = transforms.Compose(augs)
 
-    
+    # dataset
+    dataset_aug = datasets.ImageFolder("ready_to_train", transform=tfms)        
+    dataset = datasets.ImageFolder("ready_to_train", transform=None)        
 
-    
+    train_len = int(len(dataset)*0.75)
+    test_len = len(dataset)-train_len
+    trainset, _  = torch.utils.data.random_split(dataset_aug, [train_len, test_len], generator=torch.Generator().manual_seed(42))
+    _ , testset  = torch.utils.data.random_split(dataset_aug, [train_len, test_len], generator=torch.Generator().manual_seed(42))
 
+    # loaders
+    train_loader = torch.utils.data.DataLoader(
+        trainset,
+        batch_size=config.batch_size,
+        shuffle=True
+    )
+    valid_loader = torch.utils.data.DataLoader(
+        testset,
+        batch_size=config.batch_size,
+        shuffle=False
+    )
 
+    model.to(device)
+    model.train()
+   
+    # loss function 
+    criterion = nn.BCEWithLogitsLoss()
 
-    
+    # optimizer
+    optimizer = optim.AdamW(model.parameters(), lr=config.learning_rate, eps=1e-08, weight_decay=0.01)
 
+    # Train Network
+    for epoch in range(config.num_epochs):
+        print("epoch: ", epoch)
+        for batch_idx, (data, targets) in enumerate(train_loader):
+            # Get data to cuda
+            data = data.to(device=device)
+            targets = targets.to(device=device).to(torch.float32)
+
+            # forward
+            scores = model(data)
+            targets = torch.unsqueeze(targets, 1)
+            loss = criterion(scores, targets)
+            # train_loss.update(loss.item())
+
+            # backward
+            loss.backward()
+
+            optimizer.step()
+            optimizer.zero_grad()
+
+            wandb.log({
+            "train_loss": loss.item()
+            })
+
+            # train_loss.reset()
+            if (batch_idx+1)%(len(train_loader)//5) == 0:
+                evaluate(valid_loader, model)
+
+    # save model to wandb
+    torch.save(model.state_dict(), 'model.pth')
+    artifact = wandb.Artifact('model', type='model')
+    artifact.add_file('model.pth')
+    run.log_artifact(artifact)
